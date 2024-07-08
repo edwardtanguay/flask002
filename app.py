395bde6236d1d65d794c1ddb343336647824f707
Edward Tanguay (windows) <edward@tanguay.info>
2024-07-08 09:10:17 +0200

    overwrites

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import json
 import tools as t
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def welcome():
 	return render_template('welcome.html', currentTime = t.getCurrentDateTimeIso())
 
@@ -14,9 +15,13 @@ def create_url():
 @app.route('/show-url', methods=['GET', 'POST'])
 def show_url():
 	if request.method == 'POST':
+		urls = {}
+		urls[request.form['code']] = {'url': request.form['url']}
+		with open('data/urls.json', 'w') as url_file:
+			json.dump(urls, url_file)
 		return render_template('show-url.html', code=request.form['code'])
 	else:
-		return 'not valid'
+		return redirect(url_for('welcome'))
 
 port = 3737
 print(f'DEV SITE: http://localhost:{port}')
